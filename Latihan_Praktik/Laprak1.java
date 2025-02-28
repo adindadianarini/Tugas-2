f1f7e6cbb8aa90e53792ae2d52b346dff5115a3e
Adinda <adindaarini29@gmail.com>
2025-02-28 12:00:39 +0700

    Update Laprak1.java

--- Latihan_Praktik/Laprak1.java
+++ Latihan_Praktik/Laprak1.java
@@ -1,11 +1,6 @@
 public class Laprak1 {
     public static void main(String[] args) {
-        // Buat program untuk:
-        // 1. Membuat array dengan 10 bilangan bulat
-        // 2. Mengisi array dengan bilangan genap mulai dari 2
-        // 3. Menampilkan isi array
-        // 4. Menghitung jumlah semua elemen array
-
+    
         int[] bilanganGenap = new int[10];
         int jumlah = 0;
 
